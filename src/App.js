c375e7ece67c05f6aa7b63e345e58b2e1b8f8df0
16sweetyjain <34530786+16sweetyjain@users.noreply.github.com>
2020-05-25 22:26:00 +0530

    Updated App.js to render all three shelf

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 
 import './App.css'
@@ -62,44 +61,32 @@ deleteFromWant=(bookId)=>{
       }
 
 
-  addToCurrent=(bookId,catBooks)=>{
- let book=catBooks.filter(b=>b.id===bookId);
- let booki=Object.assign({},book,{id:bookId});
+  addToCurrent=(book,bookId)=>{
+ 
+let booki=[...this.state.current,{book:book,id:bookId}];
 
- //console.log(bookId);
- //console.log(booki);  
-   let books=[...this.state.current,booki];
     this.setState({
-      current:books
+      current:booki
     });
    
   }
 
-  addToWant=(bookId,catBooks)=>{
+  addToWant=(book,bookId)=>{
   
-    let book=catBooks.filter(b=>b.id===bookId);
-    //console.log(bookId);
-   // console.log(book);
-    let booki=Object.assign({},book,{id:bookId});
+   
+    let booki=[...this.state.want,{book:book,id:bookId}];
 
-
-   let books=[...this.state.current,booki];
     this.setState({
-      want:books
+      want:booki
     });
   }
 
-  addToRead=(bookId,catBooks)=>{
-    let book=catBooks.filter(b=>b.id===bookId);
-   // console.log(bookId);
-   // console.log(book);    
-    let booki=Object.assign({},book,{id:bookId});
+  addToRead=(book,bookId)=>{
+  
+    let booki=[...this.state.read,{book:book,id:bookId}];
 
- //console.log(bookId);
- //console.log(booki);  
-   let books=[...this.state.current,booki];
     this.setState({
-      read:books
+      read:booki
     });
   }
 
@@ -112,22 +99,26 @@ deleteFromWant=(bookId)=>{
   Home=()=>{
     return(
       <HomeComponent current={this.state.current}  want ={this.state.want}
-      read={this.state.read} />
+      read={this.state.read} addToCurrent={this.addToCurrent}
+      addToRead={this.addToRead} addToWant={this.addToWant}
+      deleteFromCurrent={this.deleteFromCurrent}
+      deleteFromRead={this.deleteFromRead}
+      deleteFromWant={this.deleteFromWant} />
     );
   }
    
 
   render() {
-   /* console.log('current:')
-  console.log(this.state.current);
-  console.log('read:')
- console.log(this.state.read);
- console.log('want:');
-  console.log(this.state.want);*/
-  /*this.state.current.map((book)=>{
-   console.log(book.id);
-  })*/
-  //console.log(this.state.allBooks);
+
+    
+console.log('current:')
+console.log(this.state.current);
+console.log('want:')
+console.log(this.state.want);
+console.log('read:')
+console.log(this.state.read);
+
+
     return (
       <div className="app">
         
