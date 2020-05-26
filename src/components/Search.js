3f11151d3898f93ffe16689671b120a7b5e9ff3f
16sweetyjain <34530786+16sweetyjain@users.noreply.github.com>
2020-05-26 11:19:51 +0530

    Indentation done.

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,185 +1,168 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import * as BooksAPI from '../BooksAPI'
 import { Link } from 'react-router-dom';
 
 let cat_books;
 let all_books;
 
-class Search extends Component{
+class Search extends Component {
 
-constructor(props){
-  super(props);
-  
-  this.state={
-category:null,
-categoryBooks:[],show:false,
-shelf:'art',errorInCategory:false
-  };
-  this.handleSubmit=this.handleSubmit.bind(this);
-  this.handleChange=this.handleChange.bind(this);
-  this.handleShelfChange=this.handleShelfChange.bind(this);
-  
-// this.addBooks=this.addBooks.bind(this);
+  constructor(props) {
+    super(props);
 
-}
+    this.state = {
+      category: null,
+      categoryBooks: [], show: false,
+      shelf: 'art', errorInCategory: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleShelfChange = this.handleShelfChange.bind(this);
 
 
 
+  }
 
-handleSubmit(e)
-{
- // console.log(this.state.category);
- BooksAPI.search(this.state.category).then(books=>{
+  handleChange(e) {
 
-  this.setState({categoryBooks:books})}).catch(error=>{
-   
-    this.setState({errorInCategory:true})
-    
-    
-  });
-  
+    this.setState({ category: e.target.value });
+    BooksAPI.search(this.state.category).then(books => {
 
-  
-  this.setState({show:true})
-  
-  
+      this.setState({ categoryBooks: books });
 
-e.preventDefault();
-}
-handleChange(e){
- 
-  this.setState({category:e.target.value});
-  e.preventDefault();
-}
+    }).catch(error => {
 
- 
+      this.setState({ errorInCategory: true })
 
 
+    });
 
-   handleShelfChange(book,bookId,e){
+    this.setState({ show: true })
+    e.preventDefault();
+
+  }
+
+
+  handleShelfChange(book, bookId, e) {
     // console.log(e.target.value);
-     if(e.target.value!=null)
-     this.setState({shelf:e.target.value});
-  
-     //console.log(book);
-     if(e.target.value=="currentlyReading"){
-      this.props.addToCurrent(book,bookId);
+    if (e.target.value != null)
+      this.setState({ shelf: e.target.value });
+
+    //console.log(book);
+    if (e.target.value == "currentlyReading") {
+      this.props.addToCurrent(book, bookId);
       //console.log(book);
-    }  
-    if(e.target.value=="wantToRead"){
-      this.props.addToWant(book,bookId);
     }
-    if(e.target.value=="read"){
-      this.props.addToRead(book,bookId);
+    if (e.target.value == "wantToRead") {
+      this.props.addToWant(book, bookId);
     }
- //   console.log(book); 
-   //  console.log(this.state.shelf);
-     e.preventDefault();
-   } 
+    if (e.target.value == "read") {
+      this.props.addToRead(book, bookId);
+    }
+    //   console.log(book); 
+    //  console.log(this.state.shelf);
+    e.preventDefault();
+  }
 
-  render(){
+  render() {
 
-   
-    
+    if (this.state.categoryBooks != null && this.state.errorInCategory == false) {
+      cat_books = this.state.categoryBooks.map((book) => {
 
-if(this.state.categoryBooks!=null&&this.state.errorInCategory==false){
-   cat_books=  this.state.categoryBooks.map((book) => {
+        const id = book.id;
+        let back;
+        back = book.imageLinks;
+        const isBack = back === undefined ? null : "url(" + book.imageLinks.thumbnail + ")";
+        //  this.addBooks({book});
+        let author;
+        let first = book.authors;
+        if (first !== undefined) {
+          author = (book.authors)[0];
+        }
+        else {
+          author = undefined;
+        }
 
-  const id=book.id;
-  let back;
-  back=book.imageLinks;
-  const isBack=back===undefined?null:"url("+book.imageLinks.thumbnail+")";
-   //  this.addBooks({book});
-   let author;
- let first=book.authors;
- if(first!==undefined){
-author=(book.authors)[0];
- }
- else{
-   author=undefined;
- }
-    
- 
-      return(
-        <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack}}/>
-     
-            <div className="book-shelf-changer">
-              <select  value={this.state.shelf_current} onChange={e=>this.handleShelfChange({book},id,e)}  >
-                <option value="move" >Move to...</option>
-                <option value="currentlyReading" >Currently Reading</option>
-                <option value="wantToRead" >Want to Read</option>
-                <option value="read" >Read</option>
-                <option value="none" >None</option>
-              </select>
+
+        return (
+          <li>
+            <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack }} />
+
+                <div className="book-shelf-changer">
+                  <select value={this.state.shelf_current} onChange={e => this.handleShelfChange({ book }, id, e)}  >
+                    <option value="move" >Move to...</option>
+                    <option value="currentlyReading" >Currently Reading</option>
+                    <option value="wantToRead" >Want to Read</option>
+                    <option value="read" >Read</option>
+                    <option value="none" >None</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="book-title">{book.title}</div>
+              {author === undefined ? null : <div className="book-authors">{author}</div>}
+
             </div>
-          </div>
-        
-          <div className="book-title">{book.title}</div>
-        {author===undefined?null: <div className="book-authors">{author}</div>}
-          
-    </div>
-        </li>     
-         );
-      
-        
-         
-       
+          </li>
+        );
+
+
+
+
       });
     }
-   
-  
-    
-    
-    //  console.log(this.state.categoryBooks);  
 
-      
- 
-    return(
+
+
+  
+
+
+
+    return (
       <div>
         <div>
-        <Link to='/home'>Home</Link>
+          <Link to='/home'>Home</Link>
         </div>
         <div>
-        <form onSubmit={this.handleSubmit}>
-<label>
-  Enter search text:
-  <input type="text" value={this.state.category}  onChange={this.handleChange}/>
+          <form >
+            <label>
+              Enter search text:
+  <input type="text" value={this.state.category} onChange={this.handleChange} />
 
-</label>
-<input type="submit" value="Submit"/>
-        </form>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
 
-{this.state.errorInCategory===true?(
-  <div>
-    <h1>
-      Error occured
+        {this.state.errorInCategory === true ? (
+          <div>
+            <h1>
+              Error occured
     </h1>
-    </div>
-):(
-   
- <div className="bookshelf-books">
-     
+          </div>
+        ) : (
 
-<div>
+            <div className="bookshelf-books">
 
-<ol className="books-grid">
-  
-{cat_books}
-  </ol>
 
-</div>
-</div>)}
+              <div>
 
-  </div>
+                <ol className="books-grid">
 
-    );  
-  
+                  {cat_books}
+                </ol>
 
-}
+              </div>
+            </div>)}
+
+      </div>
+
+    );
+
+
+  }
 }
 
 
