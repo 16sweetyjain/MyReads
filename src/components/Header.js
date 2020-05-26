fcb82a43f46498cc7687d4c7bd815a32e5027633
16sweetyjain <34530786+16sweetyjain@users.noreply.github.com>
2020-05-26 11:23:57 +0530

    Indentation done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+
+class Header extends Component {
+
+  render() {
+    return (
 
 
-     import React,{Component} from 'react';
-     import {
-         BrowserRouter as Router,
-         Switch,
-         Route,
-         Link
-       } from "react-router-dom";
-     class Header extends Component{
-     
-         render(){
-             return(
-     
-     
-               <div className="list-books">
-               <div className="list-books-title">
-              
-                           
-                                
-                 <h1>MyReads</h1>
-               
-               </div>
-     </div>
-     
-               );
-     
-               };
-             };
-     
-     
-         export default Header;
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+
+        </div>
+      </div>
+
+    );
+
+  };
+};
+
+
+export default Header;
+
              
