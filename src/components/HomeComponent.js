a25729944e7578b7d399607f7bae2c7ba3d334c7
16sweetyjain <34530786+16sweetyjain@users.noreply.github.com>
2020-05-29 01:13:03 +0530

    Update API fixed

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,115 +1,71 @@
 import React, { Component } from 'react';
 import * as BooksAPI from '../BooksAPI'
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 
+let curri, wanti, readi;
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      shelf_current: '',
-      shelf_read: '', shelf_want: '',
-      shelf_all: ''
-    }
-    this.handleShelfUpdateForCurrent = this.handleShelfUpdateForCurrent.bind(this);
-    this.handleShelfUpdateForRead = this.handleShelfUpdateForRead.bind(this);
-    this.handleShelfUpdateForWant = this.handleShelfUpdateForWant.bind(this);
-    this.handleShelfUpdateForAllBooks = this.handleShelfUpdateForAllBooks.bind(this);
-
-  }
-
-  handleShelfUpdateForCurrent(book, bookId, e) {
-    this.setState({ shelf_current: e.target.value });
 
 
-    this.props.deleteFromCurrent(bookId);
-    if (e.target.value == "wantToRead") {
 
-      this.props.addToWant(book, bookId);
-    }
-
-    if (e.target.value == "read") {
-      this.props.addToRead(book, bookId);
-    }
+    this.handleShelfChange = this.handleShelfChange.bind(this);
 
   }
 
 
-  handleShelfUpdateForWant(book, bookId, e) {
-    this.setState({ shelf_want: e.target.value });
-
-    this.props.deleteFromWant(bookId);
-    if (e.target.value == "currentlyReading") {
-
-      this.props.addToCurrent(book, bookId);
-    }
-
-    if (e.target.value == "read") {
-      this.props.addToRead(book, bookId);
-    }
-
-  }
+  handleShelfChange(book, id, e) {
 
 
-  handleShelfUpdateForRead(book, bookId, e) {
-    this.setState({ shelf_read: e.target.value });
 
-    //console.log(book);
-    this.props.deleteFromRead(bookId);
-    if (e.target.value == "wantToRead") {
-
-      this.props.addToWant(book, bookId);
-    }
-
-    if (e.target.value == "currentlyReading") {
-
-      this.props.addToCurrent(book, bookId);
-    }
-
-  }
-
-  handleShelfUpdateForAllBooks(book, bookId, e) {
-    this.setState({ shelf_all: e.target.value });
-
-    this.props.deleteFromAllBooks(bookId);
-
-    if (e.target.value == "currentlyReading") {
-
-      this.props.addToCurrent(book, bookId);
-    }
-
-
-    if (e.target.value == "wantToRead") {
-
-      this.props.addToWant(book, bookId);
-    }
-
-    if (e.target.value == "read") {
-
-      this.props.addToRead(book, bookId);
-    }
-
+    this.props.handleShelfUpdate(book, id, e.target.value);
+    e.preventDefault();
   }
 
 
 
 
   render() {
-    const intial = this.props.allBooks.map((book) => {
-      let back;
-      if (book == undefined) {
-        back = undefined;
-      }
-      else {
-        if (book.imageLinks == undefined) {
-          back = undefined;
-        }
-        else {
-          back = book.imageLinks;
+
+
+
+
+    const current = this.props.books.filter(b => b.shelf === 'currentlyReading');
+    //console.log(current);
+    const curri = current.map((el) => {
+
+      //const book = el.book;
+      const book = el.book != undefined ? el.book : el;
+      let currentShelf = '';
+
+
+      for (let each_book of this.props.books) {
+        const id = each_book.id != undefined ? each_book.id : each_book.book.id;
+
+        if (id === book.id) {
+          currentShelf = each_book.shelf;
+          break;
         }
       }
 
-      const isBack = "url(" + back.thumbnail + ")";
+
       const id = book.id;
+      let back;
+      back = book.imageLinks;
+      const isBack = back === undefined ? null : "url(" + book.imageLinks.thumbnail + ")";
+      //  this.addBooks({book});
+
+
+      const title = book.title ? book.title : 'No title available';
+      let author;
+      let first = book.authors;
+      if (first !== undefined) {
+        author = (book.authors)[0];
+      }
+      else {
+        author = undefined;
+      }
+
 
       return (
         <li>
@@ -118,149 +74,58 @@ class Home extends Component {
               <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack }} />
 
               <div className="book-shelf-changer">
-                <select value={this.state.shelf_all} onChange={e => this.handleShelfUpdateForAllBooks({ book }, id, e)} >
-                  <option value="move" disabled >Move to...</option>
+                <select defaultValue={currentShelf} onChange={e => this.handleShelfChanget({ book }, id, e)}   >
+                  <option value="move" >Move to...</option>
                   <option value="currentlyReading"  >Currently Reading</option>
-                  <option value="wantToRead" >Want to Read</option>
-                  <option value="read" >Read</option>
+                  <option value="wantToRead"  >Want to Read</option>
+                  <option value="read">Read</option>
                   <option value="none" >None</option>
                 </select>
               </div>
             </div>
 
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.author}</div>
-
-          </div>
-
-
-
-        </li>
-      );
-    });
-
-    const curr = this.props.current.map((el) => {
-      let back;
-      const book = el.book.book;
-
-      //  console.log(book.imageLinks);
-
-
-
-      let author, title;
-      if (book == undefined) {
-        author = undefined;
-      }
-      else {
-        if (book.authors == undefined) {
-          author = undefined;
-        }
-        else {
-          author = (book.authors)[0];
-        }
-      }
-      if (book == undefined) {
-        title = undefined;
-      }
-      else {
-        if (book.title == undefined) {
-          title = undefined;
-        }
-        else {
-          title = book.title;
-        }
-      }
-
-      if (book == undefined) {
-        back = undefined;
-      }
-      else {
-        if (book.imageLinks == undefined) {
-          back = undefined;
-        }
-        else {
-          back = book.imageLinks;
-        }
-      }
-      // console.log(book);
-
-      const isBack = back === undefined ? null : "url(" + back.thumbnail + ")";
-      const id = el.id;
-
-      return (
-        <li>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack }} />
-
-              <div className="book-shelf-changer">
-                <select value={this.state.shelf_current} onChange={e => this.handleShelfUpdateForCurrent({ book }, id, e)}  >
-                  <option value="move" >Move to...</option>
-                  <option value="currentlyReading" disabled >Currently Reading</option>
-                  <option value="wantToRead" >Want to Read</option>
-                  <option value="read" >Read</option>
-                  <option value="none" >None</option>
-                </select>
-              </div>
-            </div>
-
-            {title === undefined ? null : <div className="book-title">{title}</div>}
             {author === undefined ? null : <div className="book-authors">{author}</div>}
 
           </div>
-
-
-
         </li>
       );
+
+
+
     });
-    const read = this.props.read.map((el) => {
+    const read = this.props.books.filter(b => b.shelf === 'read');
+    // console.log(this.read);
+    const readi = read.map((el) => {
 
-      const book = el.book.book;
+      //const book = el.book;
+      const book = el.book != undefined ? el.book : el;
+      let currentShelf = '';
 
 
+      for (let each_book of this.props.books) {
+        const id = each_book.id != undefined ? each_book.id : each_book.book.id;
 
+        if (id === book.id) {
+          currentShelf = each_book.shelf;
+          break;
+        }
+      }
+
+      const id = book.id;
       let back;
-      let author, title;
-      if (book == undefined) {
+      back = book.imageLinks;
+      const isBack = back === undefined ? null : "url(" + book.imageLinks.thumbnail + ")";
+      //  this.addBooks({book});
+      let author;
+      let first = book.authors;
+      if (first !== undefined) {
+        author = (book.authors)[0];
+      }
+      else {
         author = undefined;
       }
-      else {
-        if (book.authors == undefined) {
-          author = undefined;
-        }
-        else {
-          author = (book.authors)[0];
-        }
-      }
-      if (book == undefined) {
-        title = undefined;
-      }
-      else {
-        if (book.title == undefined) {
-          title = undefined;
-        }
-        else {
-          title = book.title;
-        }
-      }
 
-
-      if (book == undefined) {
-        back = undefined;
-      }
-      else {
-        if (book.imageLinks == undefined) {
-          back = undefined;
-        }
-        else {
-          back = book.imageLinks;
-        }
-      }
-      console.log(book);
-
-      const isBack = back === undefined ? null : "url(" + back.thumbnail + ")";
-      const id = el.id;
 
       return (
         <li>
@@ -269,72 +134,57 @@ class Home extends Component {
               <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack }} />
 
               <div className="book-shelf-changer">
-                <select value={this.state.shelf_read} onChange={e => this.handleShelfUpdateForRead({ book }, id, e)}  >
+                <select defaultValue={currentShelf} onChange={e => this.handleShelfChange({ book }, id, e)}   >
                   <option value="move" >Move to...</option>
                   <option value="currentlyReading"  >Currently Reading</option>
-                  <option value="wantToRead" >Want to Read</option>
-                  <option value="read" disabled >Read</option>
+                  <option value="wantToRead"  >Want to Read</option>
+                  <option value="read">Read</option>
                   <option value="none" >None</option>
                 </select>
               </div>
             </div>
 
-            {title === undefined ? null : <div className="book-title">{title}</div>}
+            <div className="book-title">{book.title}</div>
             {author === undefined ? null : <div className="book-authors">{author}</div>}
 
           </div>
-
-
-
         </li>
       );
+
+
+
+
     });
 
-    const want = this.props.want.map((el) => {
+    const want = this.props.books.filter(b => b.shelf === 'wantToRead');
+    const wanti = want.map((el) => {
+      const book = el.book != undefined ? el.book : el;
 
-      const book = el.book.book;
+      let currentShelf = '';
 
+      for (let each_book of this.props.books) {
+        const id = each_book.id != undefined ? each_book.id : each_book.book.id;
 
+        if (id === book.id) {
+          currentShelf = each_book.shelf;
+          break;
+        }
+      }
+
+      const id = book.id;
       let back;
-      let author, title;
-      if (book == undefined) {
+      back = book.imageLinks;
+      const isBack = back === undefined ? null : "url(" + book.imageLinks.thumbnail + ")";
+      //  this.addBooks({book});
+      let author;
+      let first = book.authors;
+      if (first !== undefined) {
+        author = (book.authors)[0];
+      }
+      else {
         author = undefined;
       }
-      else {
-        if (book.authors == undefined) {
-          author = undefined;
-        }
-        else {
-          author = (book.authors)[0];
-        }
-      }
-      if (book == undefined) {
-        title = undefined;
-      }
-      else {
-        if (book.title == undefined) {
-          title = undefined;
-        }
-        else {
-          title = book.title;
-        }
-      }
 
-      if (book == undefined) {
-        back = undefined;
-      }
-      else {
-        if (book.imageLinks == undefined) {
-          back = undefined;
-        }
-        else {
-          back = book.imageLinks;
-        }
-      }
-      console.log(book);
-
-      const isBack = back === undefined ? null : "url(" + back.thumbnail + ")";
-      const id = el.id;
 
       return (
         <li>
@@ -343,36 +193,33 @@ class Home extends Component {
               <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: isBack }} />
 
               <div className="book-shelf-changer">
-                <select value={this.state.shelf_want} onChange={e => this.handleShelfUpdateForWant({ book }, id, e)}  >
+                <select defaultValue={currentShelf} onChange={e => this.handleShelfChange({ book }, id, e)}   >
                   <option value="move" >Move to...</option>
-                  <option value="currentlyReading" >Currently Reading</option>
-                  <option value="wantToRead" disabled >Want to Read</option>
-                  <option value="read" >Read</option>
+                  <option value="currentlyReading"  >Currently Reading</option>
+                  <option value="wantToRead"  >Want to Read</option>
+                  <option value="read">Read</option>
                   <option value="none" >None</option>
                 </select>
               </div>
             </div>
 
-            {title === undefined ? null : <div className="book-title">{title}</div>}
+            <div className="book-title">{book.title}</div>
             {author === undefined ? null : <div className="book-authors">{author}</div>}
 
           </div>
-
-
-
         </li>
       );
-    });
-    return (
-      <div>
-        <div className="bookshelf">
 
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-              {intial}
-            </ol>
-          </div>
-        </div>
+
+
+
+    });
+
+    return (
+
+      <div>
+
+
 
         < div className="list-books-content">
           <div>
@@ -381,7 +228,7 @@ class Home extends Component {
               <div className="bookshelf-books">
                 <ol className="books-grid">
 
-                  {curr}
+                  {curri}
                 </ol>
               </div>
             </div>
@@ -391,7 +238,7 @@ class Home extends Component {
               <h2 className="bookshelf-title">Want to Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {want}
+                  {wanti}
                 </ol>
               </div>
             </div>
@@ -401,7 +248,7 @@ class Home extends Component {
               <h2 className="bookshelf-title">Read</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  {read}
+                  {readi}
                 </ol>
               </div>
             </div>
